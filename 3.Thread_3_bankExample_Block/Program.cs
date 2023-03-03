cf6327c4cfb3a47786e30078dd0dc82d0bd2558e
acalvo <acalvo@calrom.com>
2023-03-03 09:30:45 +0000

    class made and do-While

--- 3.Thread_3_bankExample_Block/Program.cs
+++ 3.Thread_3_bankExample_Block/Program.cs
@@ -1,2 +1,65 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string finishTransaction;
+
+BankAccout myAccount = new BankAccout(2500);
+
+string endTransaction()
+{
+    Console.WriteLine("Do you want to continue with another transaction? ");
+    return finishTransaction = Console.ReadLine();
+};
+
+
+do { 
+
+Console.WriteLine("How much money do you want to get ?");
+
+double withdraw = Int32.Parse(Console.ReadLine());
+
+double resutl = myAccount.withdrawMoney(withdraw);
+
+Console.WriteLine($"Your current balance: {resutl}$");
+
+finishTransaction = endTransaction();
+
+} while (finishTransaction != "No") ;
+
+
+
+public class BankAccout
+{
+    public double money { get; set; }
+
+    public BankAccout(double money)
+    {
+        this.money = money;
+    }
+
+    public double withdrawMoney(double getMoney)
+    {
+        double currentBalance;
+
+        if (this.money < getMoney)
+        {
+            Console.WriteLine("There isn't enough money in your account, current balance");
+            return this.money;
+        }
+
+        if(this.money >= getMoney)
+        {
+            currentBalance  = this.money - getMoney;
+
+            Console.WriteLine($"Current Balance: {currentBalance}$");
+            
+            this.money = currentBalance;
+            return currentBalance;
+        }
+        
+        Console.WriteLine($"Current Balance: {this.money}$");
+        return this.money;
+    }
+
+
+
+} 
