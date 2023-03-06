67b83e7cd21bef7651c80145b0258484dcd21b0b
acalvo <acalvo@calrom.com>
2023-03-06 08:52:29 +0000

    Threads and Bank App

--- 3.Thread_3_bankExample_Block/Program.cs
+++ 3.Thread_3_bankExample_Block/Program.cs
@@ -1,35 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string finishTransaction;
+//string finishTransaction;
+
+//BankAccount class iniciate
+using System.Linq.Expressions;
 
 BankAccout myAccount = new BankAccout(2500);
 
-string endTransaction()
-{
-    Console.WriteLine("Do you want to continue with another transaction? ");
-    return finishTransaction = Console.ReadLine();
-};
+//List of Threads 
+Thread[] listThread = new Thread[10]; 
+
+//string endTransaction()
+//{
+   // Console.WriteLine("Do you want to continue with another transaction? ");
+    
+    //Each thread calling callWithraw method
+    for(int i =0; i< listThread.Length; i++) {
+        
+        Thread t = new Thread(myAccount.callWithdraw);
+        //Naming threads
+        t.Name = i.ToString();
+        listThread[i] = t;
+
+    }
+
+    //Starting threads
+    for(int i =0; i<10; i++)
+    {
+        listThread[i].Start();
+        Thread.Sleep(1000);
+    }
+
+    //return finishTransaction = Console.ReadLine();
+//};
 
 
-do { 
+//do { 
 
-Console.WriteLine("How much money do you want to get ?");
+//Console.WriteLine("How much money do you want to get ?");
 
-double withdraw = Int32.Parse(Console.ReadLine());
+//myAccount.amountWithDrawMoney = Int32.Parse(Console.ReadLine());
 
-double resutl = myAccount.withdrawMoney(withdraw);
+//finishTransaction = endTransaction();
 
-Console.WriteLine($"Your current balance: {resutl}$");
-
-finishTransaction = endTransaction();
-
-} while (finishTransaction != "No") ;
+//} while (finishTransaction != "No") ;
 
 
 
 public class BankAccout
 {
+    private Object lockThread = new Object();
     public double money { get; set; }
+
+    //public double amountWithDrawMoney { get; set; }
 
     public BankAccout(double money)
     {
@@ -42,22 +65,31 @@ public class BankAccout
 
         if (this.money < getMoney)
         {
-            Console.WriteLine("There isn't enough money in your account, current balance");
+            Console.WriteLine("There isn't enough money in your account, current balance" + Thread.CurrentThread.Name);
             return this.money;
         }
 
-        if(this.money >= getMoney)
+        //lock the thread, so only one thread get execuated each time
+        //When one is finish next get in.
+        //Lock need a object as a parameter so we created a object from Object class
+        lock (lockThread)
         {
-            currentBalance  = this.money - getMoney;
+            if (this.money >= getMoney)
+            {
+                currentBalance = this.money - getMoney;
 
-            Console.WriteLine($"Current Balance: {currentBalance}$");
-            
-            this.money = currentBalance;
-            return currentBalance;
+                this.money = currentBalance;
+            }
         }
         
-        Console.WriteLine($"Current Balance: {this.money}$");
+        Console.WriteLine($"Current Balance: {this.money}$, {Thread.CurrentThread.Name}");
         return this.money;
+    }
+
+    public void callWithdraw()
+    {
+        Console.WriteLine($"Current Thread withdrawing money: {Thread.CurrentThread.Name}");
+        withdrawMoney(500);
     }
 
 
